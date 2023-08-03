65fc58ade4fddac3971268dbe00206b8cd08aa62
soleshhaker <tobi1560@t.pl>
2023-08-03 14:40:49 +0200

    added logging to the rest of the controllers, try catch in controller methods

--- BulkyBook/BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
+++ BulkyBook/BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -5,6 +5,7 @@ using Bulky.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Serilog;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
@@ -15,6 +16,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWebHostEnvironment _webHostEnvironment;
+
         public CompanyController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
@@ -24,73 +26,152 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         [HttpGet("Index")]
         public IActionResult Index()
         {
-            IEnumerable<Company> objCompanyList = _unitOfWork.Company.GetAll();
-            return View(objCompanyList);
+            try
+            {
+                // Log that the admin accessed the company index page
+                Log.Information("Admin accessed the company index page at {Timestamp}", DateTime.Now);
+
+                IEnumerable<Company> objCompanyList = _unitOfWork.Company.GetAll();
+
+                // Log the number of companies returned
+                Log.Information("Returned {CompanyCount} company(s) at {Timestamp}", objCompanyList.Count(), DateTime.Now);
+
+                return View(objCompanyList);
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                Log.Error(ex, "An error occurred while processing the company index page at {Timestamp}", DateTime.Now);
+
+                TempData["error"] = "An error occurred while processing the company index page. Please try again later.";
+                return RedirectToAction(nameof(Index));
+            }
         }
 
         [HttpGet("Upsert/{id?}")]
         public IActionResult Upsert(int? id)
         {
-            if (id == null || id == 0)
+            try
             {
-                //create
-                var company = new Company();
-                return View(company);
+                if (id == null || id == 0)
+                {
+                    // Log that the admin accessed the company creation page
+                    Log.Information("Admin accessed the company creation page at {Timestamp}", DateTime.Now);
+
+                    // create
+                    var company = new Company();
+                    return View(company);
+                }
+                else
+                {
+                    // Log that the admin accessed the company update page
+                    Log.Information("Admin accessed the company update page for company ID {CompanyId} at {Timestamp}", id, DateTime.Now);
+
+                    // update
+                    var CompanyFromDb = _unitOfWork.Company.GetFirstOrDefault(x => x.Id == id);
+                    return View(CompanyFromDb);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                //update
-                var CompanyFromDb = _unitOfWork.Company.GetFirstOrDefault(x => x.Id == id);
-                return View(CompanyFromDb);
+                // Log the error along with the company ID
+                Log.Error(ex, "An error occurred while accessing the company page for company ID {CompanyId} at {Timestamp}", id, DateTime.Now);
+
+                TempData["error"] = "An error occurred while accessing the company page. Please try again later.";
+                return RedirectToAction(nameof(Index));
             }
         }
 
-        //POST
         [HttpPost("Upsert")]
         [ValidateAntiForgeryToken]
         public IActionResult Upsert(Company obj, IFormFile? file)
         {
-            if (ModelState.IsValid)
+            try
             {
-
-                if (obj.Id == 0)
+                if (ModelState.IsValid)
                 {
-                    _unitOfWork.Company.Add(obj);
+                    if (obj.Id == 0)
+                    {
+                        _unitOfWork.Company.Add(obj);
+                    }
+                    else
+                    {
+                        _unitOfWork.Company.Update(obj);
+                    }
+                    _unitOfWork.Save();
+
+                    // Log the company creation or update
+                    Log.Information("Company with ID {CompanyId} was {Action} at {Timestamp}", obj.Id, (obj.Id == 0 ? "created" : "updated"), DateTime.Now);
+
+                    TempData["success"] = "Company created/updated successfully";
+                    return RedirectToAction(nameof(Index));
                 }
                 else
                 {
-                    _unitOfWork.Company.Update(obj);
-
+                    return View(obj);
                 }
-                _unitOfWork.Save();
-                TempData["success"] = "Company created succesfully";
+            }
+            catch (Exception ex)
+            {
+                // Log the error along with the company ID
+                Log.Error(ex, "An error occurred while {Action} the company with ID {CompanyId} at {Timestamp}", (obj.Id == 0 ? "creating" : "updating"), obj.Id, DateTime.Now);
+
+                TempData["error"] = "An error occurred while creating/updating the company. Please try again later.";
                 return RedirectToAction(nameof(Index));
             }
-            else
-            {
-                return View(obj);
-            }
         }
+
         #region API CALLS
 
         [HttpGet("GetAll")]
         public IActionResult GetAll()
         {
-            IEnumerable<Company> objProductList = _unitOfWork.Company.GetAll();
-            return Json(new { data = objProductList });
+            try
+            {
+                IEnumerable<Company> objProductList = _unitOfWork.Company.GetAll();
+
+                // Log the number of companies returned in the API call
+                Log.Information("Returned {CompanyCount} company(s) in the API call at {Timestamp}", objProductList.Count(), DateTime.Now);
+
+                return Json(new { data = objProductList });
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                Log.Error(ex, "An error occurred in the company API call at {Timestamp}", DateTime.Now);
+
+                return Json(new { error = "An error occurred while fetching companies. Please try again later." });
+            }
         }
         [HttpDelete("Delete")]
         public IActionResult Delete(int? id)
         {
-            var companyToBeDeleted = _unitOfWork.Company.GetFirstOrDefault(x => x.Id == id);
-            if (companyToBeDeleted == null)
+            try
             {
-                return Json(new { success = false, message = "Error while deleting" });
-            }
+                var companyToBeDeleted = _unitOfWork.Company.GetFirstOrDefault(x => x.Id == id);
+                if (companyToBeDeleted == null)
+                {
+                    // Log the error when attempting to delete a company that doesn't exist
+                    Log.Error("Attempt to delete a company with ID {CompanyId} failed. Company not found at {Timestamp}", id, DateTime.Now);
 
-            _unitOfWork.Company.Remove(companyToBeDeleted);
-            _unitOfWork.Save();
-            return Json(new { success = true, message = "Delete Successful" });
+                    return Json(new { success = false, message = "Error while deleting. Company not found." });
+                }
+
+                _unitOfWork.Company.Remove(companyToBeDeleted);
+                _unitOfWork.Save();
+
+                // Log the successful deletion of the company
+                Log.Information("Company with ID {CompanyId} was deleted successfully at {Timestamp}", id, DateTime.Now);
+
+                return Json(new { success = true, message = "Delete successful" });
+            }
+            catch (Exception ex)
+            {
+                // Log the error when an exception occurs during deletion
+                Log.Error(ex, "An error occurred while deleting the company with ID {CompanyId} at {Timestamp}", id, DateTime.Now);
+
+                return Json(new { success = false, message = "An error occurred while deleting the company. Please try again later." });
+            }
         }
         #endregion
     }
--- BulkyBook/BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook/BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Serilog;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
@@ -25,162 +26,263 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         [HttpGet("Index")]
         public IActionResult Index()
         {
-            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category");
-            return View(objProductList);
+            try
+            {
+                IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+
+                // Log the number of products returned
+                int productCount = objProductList.Count();
+                Log.Information("Returned {ProductCount} product(s) in the Index page at {Timestamp}", productCount, DateTime.Now);
+
+                return View(objProductList);
+            }
+            catch (Exception ex)
+            {
+                // Log the error if an exception occurs
+                Log.Error(ex, "An error occurred while processing the Index page at {Timestamp}", DateTime.Now);
+
+                TempData["error"] = "An error occurred while processing the Index page. Please try again later.";
+                return RedirectToAction(nameof(Index));
+            }
         }
+
 
         [HttpGet("Upsert/{id?}")]
         public IActionResult Upsert(int? id)
         {
-            ProductViewModel productView = new()
+            try
             {
-                CategoryList = _unitOfWork.Category.GetAll().Select(x => new SelectListItem
+                ProductViewModel productView = new ProductViewModel
                 {
-                    Text = x.Name,
-                    Value = x.Id.ToString(),
-                }),
-                Product = new Product()
-            };
-            if (id == null || id == 0)
-            {
-                //create
-                return View(productView);
+                    CategoryList = _unitOfWork.Category.GetAll().Select(x => new SelectListItem
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString(),
+                    }),
+                    Product = new Product()
+                };
+
+                if (id == null || id == 0)
+                {
+                    // Create
+                    Log.Information("User accessed the Upsert page to create a new product at {Timestamp}", DateTime.Now);
+                    return View(productView);
+                }
+                else
+                {
+                    // Update
+                    productView.Product = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id, includeProperties: "ProductImages");
+
+                    // Log the product being updated
+                    Log.Information("User accessed the Upsert page to update product with ID {ProductId} at {Timestamp}", id, DateTime.Now);
+
+                    return View(productView);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                //update
-                productView.Product = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id, includeProperties:"ProductImages");
-                return View(productView);
+                // Log the error if an exception occurs
+                Log.Error(ex, "An error occurred while processing the Upsert page at {Timestamp}", DateTime.Now);
+
+                TempData["error"] = "An error occurred while processing the Upsert page. Please try again later.";
+                return RedirectToAction(nameof(Index));
             }
         }
 
-        //POST
+
         [HttpPost("Upsert")]
         [ValidateAntiForgeryToken]
         public IActionResult Upsert(ProductViewModel productViewModel, List<IFormFile>? files)
         {
-            if (ModelState.IsValid)
+            try
             {
-                if (productViewModel.Product.Id == 0)
+                if (ModelState.IsValid)
                 {
-                    _unitOfWork.Product.Add(productViewModel.Product);
+                    if (productViewModel.Product.Id == 0)
+                    {
+                        _unitOfWork.Product.Add(productViewModel.Product);
+                        Log.Information("Product created. Product ID: {ProductId} at {Timestamp}", productViewModel.Product.Id, DateTime.Now);
+                    }
+                    else
+                    {
+                        _unitOfWork.Product.Update(productViewModel.Product);
+                        Log.Information("Product updated. Product ID: {ProductId} at {Timestamp}", productViewModel.Product.Id, DateTime.Now);
+                    }
+                    _unitOfWork.Save();
+
+                    string wwwRootPath = _webHostEnvironment.WebRootPath;
+                    if (files != null)
+                    {
+                        foreach (IFormFile file in files)
+                        {
+                            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                            string productPath = @"images\products\product-" + productViewModel.Product.Id;
+                            string finalPath = Path.Combine(wwwRootPath, productPath);
+
+                            if (!Directory.Exists(finalPath))
+                            {
+                                Directory.CreateDirectory(finalPath);
+                            }
+
+                            using (var fileStream = new FileStream(Path.Combine(finalPath, fileName), FileMode.Create))
+                            {
+                                file.CopyTo(fileStream);
+                            }
+
+                            ProductImage productImage = new ProductImage
+                            {
+                                ImageUrl = @"\" + productPath + @"\" + fileName,
+                                ProductId = productViewModel.Product.Id,
+                            };
+
+                            if (productViewModel.Product.ProductImages == null)
+                                productViewModel.Product.ProductImages = new List<ProductImage>();
+
+                            productViewModel.Product.ProductImages.Add(productImage);
+                        }
+                        _unitOfWork.Product.Update(productViewModel.Product);
+                        _unitOfWork.Save();
+                    }
+
+                    // Log the success message and the ID of the product created/updated
+                    string action = productViewModel.Product.Id == 0 ? "created" : "updated";
+                    Log.Information("Product {Action} successfully. Product ID: {ProductId} at {Timestamp}", action, productViewModel.Product.Id, DateTime.Now);
+
+                    TempData["success"] = "Product created/updated successfully";
+                    return RedirectToAction(nameof(Index));
                 }
                 else
                 {
-                    _unitOfWork.Product.Update(productViewModel.Product);
-
-                }
-                _unitOfWork.Save();
-
-                string wwwRootPath = _webHostEnvironment.WebRootPath;
-                if(files != null)
-                {
-                    foreach(IFormFile file in files)
+                    productViewModel.CategoryList = _unitOfWork.Category.GetAll().Select(x => new SelectListItem
                     {
-                        string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-                        string productPath = @"images\products\product-" + productViewModel.Product.Id;
-                        string finalPath = Path.Combine(wwwRootPath,productPath);
+                        Text = x.Name,
+                        Value = x.Id.ToString(),
+                    });
 
-                        if (!Directory.Exists(finalPath))
-                        {
-                            Directory.CreateDirectory(finalPath);
-                        }
+                    // Log the validation failure
+                    Log.Warning("Validation failed while processing the Upsert page at {Timestamp}", DateTime.Now);
 
-                        using (var fileStream = new FileStream(Path.Combine(finalPath, fileName), FileMode.Create))
-                        {
-                            file.CopyTo(fileStream);
-                        }
-
-                        ProductImage productImage = new()
-                        {
-                            ImageUrl = @"\" + productPath + @"\" + fileName,
-                            ProductId = productViewModel.Product.Id,
-                        };
-
-                        if (productViewModel.Product.ProductImages == null)
-                            productViewModel.Product.ProductImages = new();
-
-                        productViewModel.Product.ProductImages.Add(productImage);
-                    }
-                    _unitOfWork.Product.Update(productViewModel.Product);
-                    _unitOfWork.Save();
+                    return View(productViewModel);
                 }
+            }
+            catch (Exception ex)
+            {
+                // Log the error if an exception occurs
+                Log.Error(ex, "An error occurred while processing the Upsert page at {Timestamp}", DateTime.Now);
 
-              
-                TempData["success"] = "Product created/updates succesfully";
+                TempData["error"] = "An error occurred while processing the Upsert page. Please try again later.";
                 return RedirectToAction(nameof(Index));
             }
-            else
-            {
-                productViewModel.CategoryList = _unitOfWork.Category.GetAll().Select(x => new SelectListItem
-                {
-                    Text = x.Name,
-                    Value = x.Id.ToString(),
-                });
-
-                return View(productViewModel);
-            }
         }
+
+
 
         [HttpPost("DeleteImage")]
         public IActionResult DeleteImage(int imageId)
         {
-            var imageToBeDeleted = _unitOfWork.ProductImage.GetFirstOrDefault(x => x.Id == imageId);
-            int productId = imageToBeDeleted.ProductId;
-            if(imageToBeDeleted != null)
+            ProductImage imageToBeDeleted = null;
+            try
             {
-                if (!string.IsNullOrEmpty(imageToBeDeleted.ImageUrl))
+                imageToBeDeleted = _unitOfWork.ProductImage.GetFirstOrDefault(x => x.Id == imageId);
+                int productId = imageToBeDeleted.ProductId;
+                if (imageToBeDeleted != null)
                 {
-                    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath,
-                                        imageToBeDeleted.ImageUrl.TrimStart('\\'));
-
-                    if (System.IO.File.Exists(oldImagePath))
+                    if (!string.IsNullOrEmpty(imageToBeDeleted.ImageUrl))
                     {
-                        System.IO.File.Delete(oldImagePath);
-                    }
-                }
-                _unitOfWork.ProductImage.Remove(imageToBeDeleted);
-                _unitOfWork.Save();
+                        var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath,
+                                            imageToBeDeleted.ImageUrl.TrimStart('\\'));
 
-                TempData["success"] = "Deleted successfully";
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+                    _unitOfWork.ProductImage.Remove(imageToBeDeleted);
+                    _unitOfWork.Save();
+
+                    // Log the successful deletion and the Image ID
+                    Log.Information("Image deleted successfully. Image ID: {ImageId} at {Timestamp}", imageId, DateTime.Now);
+
+                    TempData["success"] = "Deleted successfully";
+                }
+
+                return RedirectToAction(nameof(Upsert), new { id = productId });
             }
-            return RedirectToAction(nameof(Upsert), new { id = productId });
+            catch (Exception ex)
+            {
+                // Log the error if an exception occurs
+                Log.Error(ex, "An error occurred while deleting the image with ID {ImageId} at {Timestamp}", imageId, DateTime.Now);
+
+                TempData["error"] = "An error occurred while deleting the image. Please try again later.";
+                return RedirectToAction(nameof(Upsert), new { id = imageToBeDeleted?.ProductId });
+            }
         }
+
         #region API CALLS
 
         [HttpGet("GetAll")]
         [Produces("application/json")]
         public IActionResult GetAll()
         {
-            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category");
-            return Json(new { data = objProductList });
+            try
+            {
+                IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+
+                // Log the successful retrieval of all products
+                Log.Information("Successfully retrieved all products at {Timestamp}", DateTime.Now);
+
+                return Json(new { data = objProductList });
+            }
+            catch (Exception ex)
+            {
+                // Log the error if an exception occurs
+                Log.Error(ex, "An error occurred while retrieving all products at {Timestamp}", DateTime.Now);
+
+                return Json(new { data = new List<Product>() });
+            }
         }
+
         [HttpDelete("Delete/{id?}")]
         public IActionResult Delete(int? id)
         {
-            var productToBeDeleted = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id);
-            if(productToBeDeleted == null)
+            try
             {
-                return Json(new {success = false, message = "Error while deleting"});
-            }
-
-            string productPath = @"images\products\product-" + id;
-            string finalPath = Path.Combine(_webHostEnvironment.WebRootPath, productPath);
-
-            if (Directory.Exists(finalPath))
-            {
-                string[] filePaths = Directory.GetFiles(finalPath);
-                foreach (string filePath in filePaths)
+                var productToBeDeleted = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id);
+                if (productToBeDeleted == null)
                 {
-                    System.IO.File.Delete(filePath);
+                    return Json(new { success = false, message = "Error while deleting" });
                 }
-                Directory.Delete(finalPath);
-            }
 
-            _unitOfWork.Product.Remove(productToBeDeleted);
-            _unitOfWork.Save();
-            return Json(new { success = true, message = "Delete Successful" });
+                string productPath = @"images\products\product-" + id;
+                string finalPath = Path.Combine(_webHostEnvironment.WebRootPath, productPath);
+
+                if (Directory.Exists(finalPath))
+                {
+                    string[] filePaths = Directory.GetFiles(finalPath);
+                    foreach (string filePath in filePaths)
+                    {
+                        System.IO.File.Delete(filePath);
+                    }
+                    Directory.Delete(finalPath);
+                }
+
+                _unitOfWork.Product.Remove(productToBeDeleted);
+                _unitOfWork.Save();
+
+                // Log the successful deletion and the Product ID
+                Log.Information("Product deleted successfully. Product ID: {ProductId} at {Timestamp}", id, DateTime.Now);
+
+                return Json(new { success = true, message = "Delete Successful" });
+            }
+            catch (Exception ex)
+            {
+                // Log the error if an exception occurs
+                Log.Error(ex, "An error occurred while deleting the product with ID {ProductId} at {Timestamp}", id, DateTime.Now);
+
+                return Json(new { success = false, message = "An error occurred while deleting the product. Please try again later." });
+            }
         }
+
         #endregion
     }
 }
